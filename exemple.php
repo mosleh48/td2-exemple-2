d091f2aba85eefd3c17595ec7c7a11138ad65afc
mosleh48 <mosleh.hulk@gmail.com>
2023-04-12 11:15:25 +0200

    test

--- exemple.php
+++ exemple.php
@@ -1,3 +1,43 @@
 <?php
-echo "Bonjour le monde !";
-?>
+/**
+ * Template File Doc Comment
+ * 
+ * PHP version 7
+ *
+ * @category Template_Class
+ * @package  Template_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+
+/**
+ * Template Class Doc Comment
+ * 
+ * Template Class
+ * 
+ * @category Validateur_Class
+ * @package  Validateur_Class
+ * @author   Author <author@domain.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     http://localhost/
+ */
+class Validateur
+{
+    /**
+     * This function calculates the square of a number.
+     *
+     * @param int $input The number to be squared.
+     * 
+     * @return int The squared result.
+     */
+    public function check( $input ) 
+    {
+        if (strlen($input)<2) {
+            return false;
+        } else {
+            return true;       
+        }
+       
+    }   
+}
